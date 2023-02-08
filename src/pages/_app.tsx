0d73365940d0ed5044d5de212def8f69b09be310
brunof <brunof@unimedvaledocai.com.br>
2023-02-08 10:31:08 -0300

    Eslint att

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,18 @@
-import { AppProps } from "next/app";
-import Image from "next/image";
+import { AppProps } from 'next/app'
+import Image from 'next/image'
 import logo from '../assets/logo.svg'
-import { globalStyles } from "../styles/global";
-import { Container, Header } from "../styles/pages/app";
+import { globalStyles } from '../styles/global'
+import { Container, Header } from '../styles/pages/app'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-
   return (
     <Container>
-        <Header>
-             {/* eslint-disable-next-line @next/next/no-img-element */}
-            <Image src={logo} alt="" />
-        </Header>
+      <Header>
+        <Image src={logo} alt="" />
+      </Header>
       <Component {...pageProps} />
     </Container>
-      )
+  )
 }
--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -1,33 +1,36 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { stripe } from "../../lib/stripe";
+import { NextApiRequest, NextApiResponse } from 'next'
+import { stripe } from '../../lib/stripe'
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const { priceId } = req.body;
-    
-    if(req.method !== 'POST') {
-        return res.status(405).json({error: 'Method not allowed.'})
-    }
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { priceId } = req.body
 
-    if(!priceId) {
-        return res.status(400).json({error: 'Price not found.'})
-    }
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed.' })
+  }
 
-    const successUrl = `${process.env.NEXT_URL}success?session_id={CHECKOUT_SESSION_ID}`;
-    const cancelUrl = `${process.env.NEXT_URL}/`;
+  if (!priceId) {
+    return res.status(400).json({ error: 'Price not found.' })
+  }
 
-    const checkoutSession = await stripe.checkout.sessions.create({
-        success_url: successUrl,
-        cancel_url: cancelUrl,
-        mode: 'payment',
-        line_items: [
-            {
-                price: priceId,
-                quantity: 1,
-            }
-        ]
-    })
+  const successUrl = `${process.env.NEXT_URL}success?session_id={CHECKOUT_SESSION_ID}`
+  const cancelUrl = `${process.env.NEXT_URL}/`
 
-    return res.status(201).json({
-        checkoutUrl: checkoutSession.url
-    })
+  const checkoutSession = await stripe.checkout.sessions.create({
+    success_url: successUrl,
+    cancel_url: cancelUrl,
+    mode: 'payment',
+    line_items: [
+      {
+        price: priceId,
+        quantity: 1,
+      },
+    ],
+  })
+
+  return res.status(201).json({
+    checkoutUrl: checkoutSession.url,
+  })
 }
