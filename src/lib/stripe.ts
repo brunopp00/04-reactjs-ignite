fb65ff4b8e298cdb9ca0403d81a1b6508b52e0c0
brunof <brunof@unimedvaledocai.com.br>
2023-02-08 10:51:44 -0300

    Eslint att

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,10 +1,8 @@
 import Stripe from 'stripe'
-import { version } from '../../package.json'
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: '2022-11-15',
   appInfo: {
     name: 'Ignite Shop',
-    version,
   },
 })
